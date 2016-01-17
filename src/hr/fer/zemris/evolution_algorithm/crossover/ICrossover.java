c1d9a5570c09cf6e4694209cf768ff1731bdf17e
ICapalija <ivan.capalija@gmail.com>
2016-01-17 04:43:20 +0100

    Duplicate deleted

--- src/hr/fer/zemris/evolution_algorithm/crossover/ICrossover.java
+++
@@ -1,9 +0,0 @@
-package hr.fer.zemris.evolution_algorithm.crossover;
-
-import hr.fer.zemris.evolution_algorithm.chromosome.IChromosome;
-
-public interface ICrossover {
-
-	IChromosome crossover(IChromosome chrom1, IChromosome chrom2);
-
-}
--- src/hr/fer/zemris/functions/IFitnessFunction.java
+++
@@ -1,9 +0,0 @@
-package hr.fer.zemris.functions;
-
-import hr.fer.zemris.evolution_algorithm.chromosome.IChromosome;
-
-public interface IFitnessFunction {
-
-	public double calculate(IChromosome chrom);
-	
-}
