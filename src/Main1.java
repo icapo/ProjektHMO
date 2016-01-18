f044aaa6164ec47077b48bc3939c5e0b5f3453a8
icapo <ivan.capalija@hotmail.com>
2016-01-19 00:40:34 +0100

    GA call changed

--- src/Main1.java
+++ src/Main1.java
@@ -6,12 +6,9 @@ import hr.fer.tki.evolution_algorithm.task_info.TaskInfoParser;
 public class Main1 {
 
 	public static void main(String[] args) {
-		TaskInfo taskInfo = TaskInfoParser.parse("/Users/filip/Projects/hmo/ProjektHMO/sample.txt");
+		TaskInfo taskInfo = TaskInfoParser.parse("sample.txt");
 		
-		System.out.println(taskInfo.getNumberOfDays());
-		
-		GeneticAlgorithm GA = new GeneticAlgorithm(1, taskInfo);
-		
+		GeneticAlgorithm GA = new GeneticAlgorithm(null, null, null, 0, 0, 30, taskInfo);	
+		System.out.println("Population generated");
 	}
-
 }
