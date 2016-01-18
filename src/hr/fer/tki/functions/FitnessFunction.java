d151d1b8ca2f103daf0f398f3043e81ea098479a
icapo <ivan.capalija@hotmail.com>
2016-01-19 00:41:32 +0100

    Override deleted

--- src/hr/fer/tki/functions/FitnessFunction.java
+++ src/hr/fer/tki/functions/FitnessFunction.java
@@ -4,7 +4,6 @@ import hr.fer.tki.evolution_algorithm.chromosome.IChromosome;
 
 public class FitnessFunction implements IFitnessFunction {
 
-    @Override
     public double calculate(IChromosome chrom) {
         return 0;
     }
