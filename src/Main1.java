90cb54549a9c414c99ad1a1bf91e0858d7b84249
icapo <ivan.capalija@hotmail.com>
2016-01-19 01:50:35 +0100

    Run: Generate population, Write best solutions

--- src/Main1.java
+++ src/Main1.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import hr.fer.tki.evolution_algorithm.GeneticAlgorithm;
 import hr.fer.tki.evolution_algorithm.chromosome.IChromosome;
 import hr.fer.tki.evolution_algorithm.task_info.TaskInfo;
@@ -6,9 +8,15 @@ import hr.fer.tki.evolution_algorithm.task_info.TaskInfoParser;
 public class Main1 {
 
 	public static void main(String[] args) {
+		int populationSize = 30;
+		int bestSolutionsNum = 10;
 		TaskInfo taskInfo = TaskInfoParser.parse("sample.txt");
 		
-		GeneticAlgorithm GA = new GeneticAlgorithm(null, new Object(), null, 0, 0, 30, taskInfo);
+		GeneticAlgorithm GA = new GeneticAlgorithm(null, null, null, 0, 0, populationSize, taskInfo);
 		System.out.println("Population generated");
+		List<IChromosome> bestSolutions = GA.getBestSolutions(bestSolutionsNum);
+		for (int i = 0; i < bestSolutionsNum; i++) {
+			TaskInfoParser.write(bestSolutions.get(i), "res-" + (i + 1) + "-capalija.txt");
+		}
 	}
 }
